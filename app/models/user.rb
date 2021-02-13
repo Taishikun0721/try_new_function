328c80370f85e15f2378e5191af29c05086a8072
Taishiro Inokuchi <tai0721abc@gmail.com>
2021-02-13 14:50:20 +0900

    :sparkles: 必要なカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :posts, dependent: :destroy
   validates :name, presence: true
   validates :age, presence: true
+  validates :email, presence: true, uniqueness: true
 end
